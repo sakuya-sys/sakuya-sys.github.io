397663a1fd7f18a200ae1bff0a53d314e83f8359
S@Ku_γA <3592489808@qq.com>
2026-06-01 20:32:41 +0800

    Site updated: 2026-06-01 20:32:31

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1778849996747";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1780317003242";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
